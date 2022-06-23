b381d759b2edae841f1ddb08d185a8517f112614
JunioVeras <47642059+JunioVeras@users.noreply.github.com>
2022-06-23 14:24:25 -0300

    Add files via upload

--- main2.cpp
+++ main2.cpp
@@ -5,6 +5,7 @@
 // ---------------------------------------------------------------------
 
 #include <iostream>
+#include <stack>
 
 struct Piso
 {
@@ -18,7 +19,6 @@ struct Mesa
     int largura;
 };
 
-
 class Casa
 {
     private:
@@ -26,6 +26,7 @@ class Casa
         Piso **planta;
         Mesa *mesas;
         int *retangulos;
+        void insere_retangulo(int altura, int largura);
 
     public:
         Casa();
@@ -101,21 +102,79 @@ void Casa::calcula_alturas()
     }
 }
 
+void Casa::insere_retangulo(int altura, int largura)
+{
+    if(this->retangulos[altura] < largura)
+    {
+        this->retangulos[altura] = largura;
+    }
+}
+
 void Casa::calcula_retangulos()
 {
     for(int i = this->N-1; i >=0; i--)
     {
         for(int j = 0; j < this->M; j++)
         {
-            
+            if(this->planta[i][j].disponivel == 0)
+            {
+                continue;
+            }
+            int compativel_esquerda = 0;
+            int compativel_direita = this->M - 1;
+            for(int k = j - 1; k >= 0; k--)
+            {
+                if(this->planta[i][k].altura < this->planta[i][j].altura)
+                {
+                    compativel_esquerda = k + 1;
+                    break;
+                }
+            }
+            for(int k = j + 1; k < this->M; k++)
+            {
+                if(this->planta[i][k].altura < this->planta[i][j].altura)
+                {
+                    compativel_direita = k - 1;
+                    break;
+                }
+            }
+            insere_retangulo(this->planta[i][j].altura, compativel_direita - compativel_esquerda + 1);
         }
     }
 }
 
+bool operator>(Mesa mesa1, Mesa mesa2)
+{
+    if(mesa1.comprimento * mesa1.largura > mesa2.comprimento * mesa2.largura)
+    {
+        return 1;
+    }
+    return 0;
+}
+
 void Casa::verifica_mesas()
 {
-
-    // std::cout << maior_mesa.largura << " " << maior_mesa.comprimento;
+    Mesa maior_mesa;
+    maior_mesa.comprimento = 0;
+    maior_mesa.largura = 0;
+    for(int i = 0; i < this->K; i++)
+    {
+        if(this->mesas[i].comprimento <= this->N)
+        {
+            if(this->retangulos[this->mesas[i].comprimento] >= this->mesas[i].largura)
+            {
+                maior_mesa = (this->mesas[i] > maior_mesa) ? this->mesas[i] : maior_mesa;
+            }
+        }
+        if(this->mesas[i].largura <= this->N)
+        {
+            if(this->retangulos[this->mesas[i].largura] >= this->mesas[i].comprimento)
+            {
+                maior_mesa = (this->mesas[i] > maior_mesa) ? this->mesas[i] : maior_mesa;
+            }
+        }
+    }
+    std::cout << maior_mesa.largura << " " << maior_mesa.comprimento;
 }
 
 void Casa::imprimir()
@@ -128,6 +187,12 @@ void Casa::imprimir()
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
+    for(int i = 0; i <= this->N; i++)
+    {
+        std::cout << i << " " << this->retangulos[i];
+        std::cout << std::endl;
+    }
 }
 
 int main()
@@ -137,7 +202,8 @@ int main()
 {
     Casa casa;
     casa.calcula_alturas();
+    casa.calcula_retangulos();
+    // casa.imprimir();
     casa.verifica_mesas();
-    casa.imprimir();
     return 0;
 }
